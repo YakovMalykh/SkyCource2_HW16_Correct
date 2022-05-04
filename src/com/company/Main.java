ce32a11739c956e8f0313225f208be01fa530c64
yakov <to-yakoff@mail.ru>
2022-05-04 20:43:51 +0300

    выполнил ДЗ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        StringListImpl strings = new StringListImpl(5);
+        StringListImpl secondStrings = new StringListImpl(5);
+        System.out.println(strings.add("www"));
+        System.out.println(strings.add("rrr"));
+        System.out.println(strings.add("aaa"));
+//        System.out.println(strings.add("aaa"));
+        System.out.println(strings);
+//        System.out.println(strings.add(2,"jjj"));
+//        System.out.println(strings);
+
+        System.out.println(secondStrings.add("www"));
+        System.out.println(secondStrings.add("rrr"));
+        System.out.println(secondStrings.add("aaa"));
+        System.out.println(secondStrings);
+
+        System.out.println(strings.equals(secondStrings));
     }
 }
